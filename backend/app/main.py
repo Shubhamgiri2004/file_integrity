5c26cee0abb21f6770555e4b9fd65222ce25f0cf
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2026-03-11 09:58:08 +0530

    update in cors origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -119,7 +119,8 @@ app.add_middleware(
 		"http://localhost:5173",
 		"http://localhost:3000",
 		"http://127.0.0.1:5173",
-		"http://127.0.0.1:3000"
+		"http://127.0.0.1:3000",
+		"https://file-integrity-4d44.vercel.app/"
 	],
 	allow_credentials=True,
 	allow_methods=["*"],
